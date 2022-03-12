140b22f90d406ef6170e1ea6b52d26860117ffe1
Eoin Mcloughlin <eoin.mclouglin1997@gmail.com>
2022-03-12 15:31:14 +0000

    Added save results button for saving the results so I dont have to do it all at once.

--- tkinter_gui.py
+++ tkinter_gui.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import cv2
+import json
 import tkinter as tk
 import tkinter.filedialog
 
@@ -36,24 +37,13 @@ class DataProcessingTool:
         self.master = master
         master.title("MoveAhead Data Processing Tool")
 
-        # # Load MoveAhead Logo into GUI
-        # image = Image.open("moveahead_logo.png")
-        # image = Image.open(get_path("moveahead_logo.png"))
-        # image = image.resize((150, 40), Image.ANTIALIAS)
-
-        # logo = ImageTk.PhotoImage(image)
-
-        # self.label = tk.Label(master, image=logo)
-        # self.label.photo = logo
-        # self.label.pack()
-
-        # Class variables
-        self.start_date = None
-        self.end_date = None
-
+        self.drip_map = {}
         # Place buttons for additional functionality
         self.select_folder_button = tk.Button(master, text="Select Folder", command=self.select_folder)
-        self.select_folder_button.place(x=25, y=50)
+        self.select_folder_button.place(x=5, y=20)
+
+        self.save_drip_map_button = tk.Button(master, text="Save Results", command=self.save_json)
+        self.save_drip_map_button.place(x=5, y=80)
 
     def select_folder(self):
         folder_path = tk.filedialog.askdirectory()
@@ -70,39 +60,64 @@ class DataProcessingTool:
             # Add file name to folder
             folder_contents.append(item)
 
-        # create listbox object
-        listbox = tk.Listbox(width=max_len+2, bg = "white", activestyle = 'dotbox', fg = "black")
+        # # create listbox object
+        # listbox = tk.Listbox(width=max_len+2, bg = "white", activestyle = 'dotbox', fg = "black")
 
-        # Populate listbox with contents from the selected folder
-        for index, item in enumerate(folder_contents):
-            listbox.insert(index, item)
+        # # Populate listbox with contents from the selected folder
+        # for index, item in enumerate(folder_contents):
+        #     listbox.insert(index, item)
 
-        listbox.place(x=25, y=80)
+        # listbox.place(x=25, y=80)
         
         self.start_processing_button = tk.Button(self.master, text="Start Processing", command=self.process_folder)
-        self.start_processing_button.place(x=125, y=80)
+        self.start_processing_button.place(x=200, y=50)
+
+    def save_json(self):
+
+        def import_json(json_path):
+            with open(json_path, "r") as f:
+                json_data = json.load(f)
+
+            return json_data
+
+        def add_results(drip_map, exists):
+            if exists:
+                logged_results = import_json("results.json")
+            else:
+                logged_results = {} 
+
+            print("HERE:", drip_map)
+            merged_logs = {**drip_map, **logged_results}
+
+            with open("results.json", mode='w+') as json_file:
+                json.dump(merged_logs, json_file)
+
+            return
+
+        if path.exists("results.json"):
+            add_results(self.drip_map, True)
+        else:
+            add_results(self.drip_map, False)
+
+
 
     def process_folder(self):
 
+        # def get_results_file():
+        #     if path.exists("results.json"):
+        #         log = import_json("results.json")
+        #     else:
+        #         log = {}
+
+        #     return log
+
+        # self.log = get_results_file()
         dset_path = os.listdir("clothes_dataset/")
-
-        def get_results_file():
-            if path.exists("results.json"):
-                log = import_json("results.json")
-            else:
-                log = {}
-
-            return log
-
-        self.log = get_results_file()
 
         for image_name in os.listdir(self.folder_path):
             img_path = self.folder_path + "/" + image_name
             if image_name in dset_path:
-                t = self.temp_video_gui(img_path, image_name)
-                print(t)
-            
-                # print(image_name)
+                self.temp_video_gui(img_path, image_name)
 
         # Close GUI once uploads have been completed.
         self.master.destroy()
@@ -112,10 +127,12 @@ class DataProcessingTool:
 
         # Initialize second GUI to handle video annotation and processing (tk.Toplevel since another GUI instance is already running)
         temp_gui = tk.Toplevel()
-        test = selff.master
 
         pad = 3
         temp_gui.geometry("{0}x{1}+0+0".format(temp_gui.winfo_screenwidth()-pad, temp_gui.winfo_screenheight()-pad))
+
+        # selff.save_drip_map_button = tk.Button(master, text="Save Results", command=self.save_json)
+        # selff.save_drip_map_button.place(x=400, y=50)
 
         temp_gui.resizable(0, 0)
         temp_gui.title(image_name) 
@@ -124,19 +141,20 @@ class DataProcessingTool:
             # Load Dataset Image into GUI
             dataset_image = Image.open(output_path)
                 
-                # Get image dimensions
+            # Get image dimensions
             img = cv2.imread(output_path)
             img_height, img_width, _ = img.shape
             screen_width = GetSystemMetrics(0)
             screen_height = GetSystemMetrics(1)
 
-                # Get ratio of image height to screen height 
+            # Get ratio of image height to screen height 
             ratio = screen_height / img_height 
 
             dataset_image = dataset_image.resize(( int(img_width * ratio), int(img_height * ratio)), Image.ANTIALIAS)
             tk_dataset_img = ImageTk.PhotoImage(dataset_image)
 
-                # Create canvas to display image
+
+            # Create canvas to display image
             canvas = Canvas(temp_gui, width = GetSystemMetrics(0), height = GetSystemMetrics(1))  
             canvas.pack()
             canvas.create_image(20, 20, anchor=NW, image= tk_dataset_img)  
@@ -144,48 +162,42 @@ class DataProcessingTool:
             l2 = tk.Label(temp_gui, text="ID: {}".format(image_name), font=("Arial", 20))
             l2.place(x=800, y=120)
 
-            def create_folder(folder_name):
-                # used to create folders
-                if not os.path.exists(folder_name):
-                    os.mkdir(folder_name)
-
-                return
-
-            def move_image(folder_name):
-                dest_path = folder_name + image_name
-                # print("Moving to:", dest_path)
-
 
             def leftKey(event):
-                create_folder("labelled_dataset/not_drip/")
-                move_image("labelled_dataset/not_drip/")
-                print("LEFT:", image_name)
+                selff.drip_map[image_name] = "not_drip"
                 temp_gui.destroy()
 
             def rightKey(event):
-                create_folder("labelled_dataset/drip/")
-                move_image("labelled_dataset/drip/")
-                print("RIGHT", image_name)
-                drip = True
-                print(drip)
+                selff.drip_map[image_name] = "drip"
+                print(selff.drip_map)
                 temp_gui.destroy()
 
-            create_folder("labelled_dataset/")
-            frame = Frame(test, width=500, height=500)
-            drip = False
-            test.bind('<Left>', leftKey)
-            test.bind('<Right>', rightKey)
+            frame = Frame(selff.master, width=500, height=500)
+            selff.master.bind('<Left>', leftKey)
+            selff.master.bind('<Right>', rightKey)
             frame.pack()
 
                 # Wait until the window is closed to open the next
             temp_gui.wait_window()
-            return drip
 
         except:
             temp_gui.destroy()
-    
+ 
+
+def create_folder(folder_name):
+    # used to create folders
+    if not os.path.exists(folder_name):
+        os.mkdir(folder_name)
+
+    return
+
 
 def main():
+
+    # Create class folders
+    create_folder("labelled_dataset/")
+    create_folder("labelled_dataset/not_drip/")
+    create_folder("labelled_dataset/drip/")
     # Load Tkinter root window
     window = tk.Tk()
     window.geometry("600x300")
